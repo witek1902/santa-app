2568a828f6cf1b89011920069584a3439bdcffa5
Artur Czopek <arturcz32@gmail.com>
2017-11-09 10:55:42 +0100

    - added login to remote

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'AIzaSyBtU89EYN1TCsvnSpuMwaTE4pIJyVck6bs',
+  authDomain: 'santa-app-fe86a.firebaseapp.com',
+  databaseURL: 'https://santa-app-fe86a.firebaseio.com',
+  projectId: 'santa-app-fe86a',
+  storageBucket: 'santa-app-fe86a.appspot.com',
+  messagingSenderId: '100487748165'
 };
